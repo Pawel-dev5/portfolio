92e8033b0cca9d37fcb018be590fea1ce6aa1022
Pawel-dev5 <p.nowecki@gmail.com>
2020-10-17 23:29:36 +0200

    wzor, niedzialajace funkcje

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,52 +5,9 @@ const ScrollLink = Scroll.Link;
 
 const Navigation = () => {
     return (
-        <div className="NavBox">
-            <h3 id="name" >Paweł Nowecki</h3>
-            <Nav className="menu-link"
-                 activeKey="/home">
-                <Nav.Item>
-                    <ScrollLink
-                        className="navy"
-                        smooth={true}
-                        duration={500}
-                        to="FourSteps"
-                        // eventKey="Active"
-                    >Active
-                </ScrollLink>
-                </Nav.Item>
-                <Nav.Item>
-                    <ScrollLink
-                        className="navy"
-                        smooth={true}
-                        duration={500}
-                        to="FourSteps"
-                        // eventKey="link-1"
-                    >Link
-                </ScrollLink>
-                </Nav.Item>
-                <Nav.Item>
-                    <ScrollLink
-                        className="navy"
-                        smooth={true}
-                        duration={500}
-                        to="FourSteps"
-                        // eventKey="link-2"
-                    >Link
-                </ScrollLink>
-                </Nav.Item>
-                <Nav.Item>
-                    <ScrollLink
-                        className="navy"
-                        smooth={true}
-                        duration={500}
-                        to="FourSteps"
-                        // eventKey="disabled"
-                    >Disabled
-                </ScrollLink>
-                </Nav.Item>
-            </Nav>
-        </div>
+        <>
+        
+        </>
     )
 }
 
