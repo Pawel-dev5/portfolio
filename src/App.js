b04275c1e99189d077feabaefbf0cde50008d338
Pawel-dev5 <p.nowecki@gmail.com>
2020-10-17 16:07:16 +0200

    Zrobione cos zmiana layoutu

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
-import "~bootstrap/scss/bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navigation from "../src/components/Nav";
+// import Nav from 'react-bootstrap/Nav';
+import {Button} from 'react-bootstrap';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+    <Navigation/>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
+        </header>
+      </div>
+      <Button variant="primary" size="lg" block>
+    Block level button
+  </Button>
+  <Button id="FourSteps" variant="secondary" size="lg" block>
+    Block level button
+  </Button>
+    </>
   );
 }
 
